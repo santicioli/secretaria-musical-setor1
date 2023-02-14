248850c6d38bd6bc2f889c63786fc971cc3695b4
Johnata Santicioli <johnata.santicioli@gmail.com>
2023-02-14 14:59:52 -0300

    add às

--- main.js
+++ main.js
@@ -1,191 +1,191 @@
 data = [
     {church: '<a href="https://goo.gl/maps/G7Qn7CaX9ryRgVPN8">Agua Rasa</a>', 
         //gem:{day: '', hour:''},
-        local:{day: '3º sáb. 17:00h', hour: ''}
+        local:{day: '3º sáb. às 17:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/oZWGJamfHD1yu36K6">Americanópolis</a>', 
         //gem:{day: '', hour:''},
-        local:{day: '1º sáb. 17:00h', hour: ''}
+        local:{day: '1º sáb. às 17:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/q38firHnsG44yPJS9">Baixada do Glicério</a>', 
         //gem:{day: '', hour:''},
-        local:{day: '1º sáb. 17:00h', hour: ''}
+        local:{day: '1º sáb. às 17:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/h5k5VATu1q4Dbncz6">Barra Funda</a>', 
         //gem:{day: '', hour:''},
-        local:{day: 'último dom. 16:00h', hour: ''}
+        local:{day: 'último dom. às 16:00h', hour: ''}
     },
 	{church: '<a href="https://goo.gl/maps/gp4VdETQsbRZiH4x9">Belém</a>', 
-        gem:{day: '2ª sex. 19:30h', hour: ''},
-        local:{day: '4º sáb. 19:30h', hour: ''},
+        gem:{day: '2ª sex. às 19:30h', hour: ''},
+        local:{day: '4º sáb. às 19:30h', hour: ''},
     },
 	{church: '<a href="https://goo.gl/maps/DCdi4MBDcd7aV8TW8">Bom Retiro</a>',  
-        gem:{day: 'todo o dom. 09:10h', hour: ''},
-        local:{day: '1º sáb. 17:00h', hour: ''}
+        gem:{day: 'todo o dom. às 09:10h', hour: ''},
+        local:{day: '1º sáb. às 17:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/YiyrRsnXudyENdie6">Bosque da Saúde</a>', 
         //gem:{day: '', hour:''},
-        local:{day: 'último dom. 16:00h', hour: ''}
+        local:{day: 'último dom. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/u93cJ55CEZTMGcrLA">Brás</a>', 
         //gem:{day: '', hour:''},
-        local:{day: '4º dom. 16:00h', hour: ''}
+        local:{day: '4º dom. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/nxgr3wYUK98JD9aH7">Canindé</a>', 
         //gem:{day: '', hour:''},
-        local:{day: '2º sáb. 16h30h', hour: ''}
+        local:{day: '2º sáb. às 16h30h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/nsYJQV9yiVCrbrHm8">Caraguatá</a>', 
-        gem:{day: '1º sáb. 15:30h <b>(final do GEM)</b>', hour: ''},
-        local:{day: '1º dom. 16:00h', hour: ''}
+        gem:{day: '1º sáb. às 15:30h <b>(final do GEM)</b>', hour: ''},
+        local:{day: '1º dom. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/PfxX3rWpAdvDpepP9">Flor da Vila Formosa</a>', 
         //gem:{day: '', hour:''},
-        local:{day: '3º sáb. 17:00h', hour: ''}
+        local:{day: '3º sáb. às 17:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/j5YGNgbdPyehFMUi6">Ipiranga</a>', 
         //gem:{day: '', hour:''},
-        local:{day: '4º sáb. 16:00h', hour: ''}
+        local:{day: '4º sáb. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/x1dM8vfsD8CH9LRbA">Jabaquara</a>', 
-        gem:{day: 'última qui. 19:30h', hour: ''},
-        local:{day: 'último sáb. 19:30h', hour: ''}
+        gem:{day: 'última qui. às 19:30h', hour: ''},
+        local:{day: 'último sáb. às 19:30h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/x1dM8vfsD8CH9LRbA">Jardim da Glória</a>', 
-        gem:{day: '2º sáb. 17:00h', hour:''},
-        local:{day: '4º dom. 16:00h', hour: ''}
+        gem:{day: '2º sáb. às 17:00h', hour:''},
+        local:{day: '4º dom. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/oBvAgiFfnpestN4c8">Jardim Elba</a>', 
-        gem:{day: '2º sáb. 15:00h', hour: ''},
-        local:{day: '4º dom. 16:00h', hour: ''}
+        gem:{day: '2º sáb. às 15:00h', hour: ''},
+        local:{day: '4º dom. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/h6uKTbYrhtYYaYVHA">Jardim Independência</a>', 
         //gem:{day: '', hour:''},
-        local:{day: '2º sáb. 19:30h', hour: ''}
+        local:{day: '2º sáb. às 19:30h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/LfUxnUYqHgKgxWBZ9">Jardim Maria Estela</a>', 
-        gem:{day: '3º sáb. 16:00h', hour: ''},
-        local:{day: '4º dom. 16:00h', hour: ''}
+        gem:{day: '3º sáb. às 16:00h', hour: ''},
+        local:{day: '4º dom. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/csf7kQ4wHnYJczLAA">Jardim Mimar</a>', 
-        gem:{day: '3ª ter. 20:00h', hour: ''},
-        local:{day: '3º sáb. 19:30h', hour: ''}
+        gem:{day: '3ª ter. às 20:00h', hour: ''},
+        local:{day: '3º sáb. às 19:30h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/Cwfc231pLXe2SJHp8">Jardim Panorama</a>', 
-        gem:{day: '3º sáb. 14:30h', hour: ''},
-        local:{day: 'último dom. 16:00h', hour: ''}
+        gem:{day: '3º sáb. às 14:30h', hour: ''},
+        local:{day: 'último dom. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/umzXqZ6dCfM4BEhj9">Jardim Planalto</a>', 
-        gem:{day: '3ª sex. 20:00h', hour: ''},
-        local:{day: '1º dom. 16:00h', hour: ''}
+        gem:{day: '3ª sex. às 20:00h', hour: ''},
+        local:{day: '1º dom. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/64iYaNrSfnaQ94uE7">Jardim Santo Eduardo</a>', 
-        gem:{day: '3ª sex. 19:30h', hour: ''},
-        local:{day: '1º sáb. 17:00h', hour: ''}
+        gem:{day: '3ª sex. às 19:30h', hour: ''},
+        local:{day: '1º sáb. às 17:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/64iYaNrSfnaQ94uE7">Jardim São Roberto</a>', 
         //gem:{day: '', hour:''},
-        local:{day: '1º sáb. 19:30h', hour: ''}
+        local:{day: '1º sáb. às 19:30h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/45xcGxTEdY7NPyabA">Jardim São Roberto</a>', 
-        gem:{day: '1º sáb. 10:30h <b>(final do GEM)</b>', hour: ''},
-        local:{day: 'último dom. 16:00h', hour: ''}
+        gem:{day: '1º sáb. às 10:30h <b>(final do GEM)</b>', hour: ''},
+        local:{day: 'último dom. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/jKsr23T8JJPRZa7R6">Jardim Yara</a>', 
         //gem:{day: '', hour:''},
-        local:{day: '3º sáb. 19:30h', hour: ''}
+        local:{day: '3º sáb. às 19:30h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/SQCfco2fdc1QDnTi6">Parque Santa Madalena</a>', 
-        gem:{day: 'última qui. 20:00h', hour: ''},
-        local:{day: '4º sex. 20:00h', hour: ''}
+        gem:{day: 'última qui. às 20:00h', hour: ''},
+        local:{day: '4º sex. às 20:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/SD2DpycwXu1JkHjx8">Parque São Lucas</a>', 
-        gem:{day: 'última qua. 19:30h', hour: ''},
-        local:{day: '1º sáb. 16:30h', hour: ''}
+        gem:{day: 'última qua. às 19:30h', hour: ''},
+        local:{day: '1º sáb. às 16:30h', hour: ''}
     },
     {church: 'Parque Sevilha', 
         //gem:{day: '', hour:''},
         local:{day: 'Não tem ensaio', hour:''}
     },
     {church: '<a href="https://goo.gl/maps/PA9dgQGcpQz6LpBE6">Ponte Pequena</a>', 
-        gem:{day: '1º sáb. 15:00h', hour: ''},
-        local:{day: '3º qua. 19:30h', hour: ''}
+        gem:{day: '1º sáb. às 15:00h', hour: ''},
+        local:{day: '3º qua. às 19:30h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/PA9dgQGcpQz6LpBE6">São João Clímaco</a>', 
         //gem:{day: '', hour:''},
-        local:{day: '4º sáb. 17:00h', hour: ''}
+        local:{day: '4º sáb. às 17:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/snikN2xpT5XDMj1LA">Sapopemba</a>', 
         //gem:{day: '', hour:''},
-        local:{day: '1º sex. 19:30h', hour: ''}
+        local:{day: '1º sex. às 19:30h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/qH8TFubXB8QwQN57A">Sítio Pinheirinho</a>', 
         //gem:{day: '', hour:''},
-        local:{day: '1º sáb. 19:30h', hour: ''}
+        local:{day: '1º sáb. às 19:30h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/9oS1haexWvDVWzW3A">Vila Alpina</a>', 
-        gem:{day: '3ª sex. 19:30h', hour: ''},
-        local:{day: '4º dom. 16:00h', hour: ''}
+        gem:{day: '3ª sex. às 19:30h', hour: ''},
+        local:{day: '4º dom. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/27V1jJiFeu8gJTPB6">Vila Arapuá</a>', 
         //gem:{day: '', hour:''},
-        local:{day: '1º dom. 16:00h', hour: ''}
+        local:{day: '1º dom. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/SVZoZkWf3p7i5wif9">Vila Bela</a>', 
         //gem:{day: '', hour:''},
-        local:{day: '1º sex. 19:30h', hour: ''}
+        local:{day: '1º sex. às 19:30h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/SVZoZkWf3p7i5wif9">Vila California</a>', 
-        gem:{day: 'última qua. 20:00h', hour: ''},
-        local:{day: '4º dom. 16:00h', hour: ''}
+        gem:{day: 'última qua. às 20:00h', hour: ''},
+        local:{day: '4º dom. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/cKUZJHp1abKdBksx6">Vila Carioca</a>', 
-        gem:{day: '1ª seg. 19:30h', hour: ''},
-        local:{day: '4º dom. 16:00h', hour: ''}
+        gem:{day: '1ª seg. às 19:30h', hour: ''},
+        local:{day: '4º dom. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/524qYBqrHHKXzVHGA">Vila Clara</a>', 
         //gem:{day: '', hour:''},
-        local:{day: '4º dom. 16:00h', hour: ''}
+        local:{day: '4º dom. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/utTnvA1AbMqRy87CA">Vila Diva</a>', 
-        gem:{day: 'todo o sáb. 16:00h <b>(final do GEM)</b>', hour: ''},
-        local:{day: 'último sáb. 17:00h', hour: ''}
+        gem:{day: 'todo o sáb. às 16:00h <b>(final do GEM)</b>', hour: ''},
+        local:{day: 'último sáb. às 17:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/8iaoff3gQGLGvUSH8">Vila Ema</a>', 
-        gem:{day: 'última ter. 19:30h', hour: ''},
-        local:{day: '2º sáb. 19:30h', hour: ''}
+        gem:{day: 'última ter. às 19:30h', hour: ''},
+        local:{day: '2º sáb. às 19:30h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/3uomPagvm6kXvm84A">Vila Formosa</a>', 
-        gem:{day: 'último dom. 09:00h', hour: ''},
-        local:{day: '1º dom. 16h30h', hour: ''}
+        gem:{day: 'último dom. às 09:00h', hour: ''},
+        local:{day: '1º dom. às 16h30h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/N3BFzosEHCgZuk1t6">Vila Guarani</a>', 
-        gem:{day: 'todo o sáb. 16:30h <b>(final do GEM)</b> </br> <span class="red">2º Sábado ensaio local às 17:00h</span>', hour: ''},
-        local:{day: '2º sáb. 17:00h', hour: ''}
+        gem:{day: 'todo o sáb. às 16:30h <b>(final do GEM)</b> </br> <span class="red">2º Sábado ensaio local às 17:00h</span>', hour: ''},
+        local:{day: '2º sáb. às 17:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/XiQgY2V7DPtrQeVK8">Vila Independência</a>', 
         //gem:{day: '', hour:''},
-        local:{day: '4º dom. 16:00h', hour: ''}
+        local:{day: '4º dom. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/gGU86mzDkABGvJLV6">Vila Livieiro</a>', 
-        gem:{day: '3ª seg. 19:30h', hour: ''},
-        local:{day: 'último dom. 16:00h', hour: ''}
+        gem:{day: '3ª seg. às 19:30h', hour: ''},
+        local:{day: 'último dom. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/7G3yLP3jbkZUayz89">Vila Mariana</a>', 
         //gem:{day: '', hour:''},
-        local:{day: 'último dom. 16:00h', hour: ''}
+        local:{day: 'último dom. às 16:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/LvpTgn7gaPMuFzcm9">Vila Moraes</a>', 
         //gem:{day: '', hour:''},
-        local:{day: 'último sáb. 17:00h', hour: ''}
+        local:{day: 'último sáb. às 17:00h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/JnjjXa4nJMhesvJd8">Vila Prudente</a>', 
-        gem:{day: 'última seg. 19:30h', hour: ''},
-        local:{day: '2º sex. 19:30h', hour: ''}
+        gem:{day: 'última seg. às 19:30h', hour: ''},
+        local:{day: '2º sex. às 19:30h', hour: ''}
     },
     {church: '<a href="https://goo.gl/maps/D8KR16teYmprGFBn6">Vila Rio Branco</a>', 
-        gem:{day: 'toda a seg. 20:30h <b>(final do GEM)</b>', hour: ''},
-        local:{day: '1º dom. 16:00h', hour: ''}
+        gem:{day: 'toda a seg. às 20:30h <b>(final do GEM)</b>', hour: ''},
+        local:{day: '1º dom. às 16:00h', hour: ''}
     },
 ]
 
